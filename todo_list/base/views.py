9b522f199764ccaec74732786c0cfc0f2e28b14a
David King <leanmachine42@yahoo.com>
2021-10-04 16:00:23 -0500

    crud operations complete for tasks

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -18,4 +18,13 @@ class TaskCreate(CreateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("tasks")
-    
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+ 
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = "task"
+    success_url = reverse_lazy("tasks")
