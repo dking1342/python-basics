5c08301de56213b265cb28211f5a834ff59626b7
David King <leanmachine42@yahoo.com>
2021-10-05 16:17:19 -0500

    created single page views

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -16,14 +16,16 @@
           Latest products
         </h2>
       </div>
-      <div class="column is-4" v-for="product in latestProducts" :key="product.id">
-        <div class="box">
-          <figure class="image mb-4">
-            <img :src="product.get_thumbnail" :alt="product.name">
-          </figure>
-          <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-          View Details
+      <div class="column is-12 is-flex is-desktop">
+        <div class="column is-4 " v-for="product in latestProducts" :key="product.id">
+          <div class="box">
+            <figure class="image mb-4">
+              <img :src="product.get_thumbnail" :alt="product.name">
+            </figure>
+            <h3 class="is-size-4">{{ product.name }}</h3>
+            <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+            <router-link :to="product.get_absolute_url" class="button is-dark mt-4" >View Details</router-link>
+          </div>
         </div>
       </div>
     </div>
